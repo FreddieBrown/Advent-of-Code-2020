2f2ca5fe2e5bfebf82fbe41b5fe5b53e4da96a7a
Freddie Brown <fred@noser.net>
2020-12-01 13:30:22 +0000

    day 1

---
+++ day1.py
@@ -0,0 +1,23 @@
+in_list = []
+seen = set()
+
+with open('day1.txt') as f:
+    for line in f:
+        in_list.append(int(line))
+
+
+print("Part 1")
+for item in in_list:
+    for elem in seen:
+        if item + elem == 2020:
+            print(str(item * elem))
+    seen.add(item)
+
+print("Part 2")
+for item in in_list:
+    for i in seen:
+        for j in seen:
+            if item + i + j == 2020: 
+                print(str(item * i * j))
+    seen.add(item)
+
