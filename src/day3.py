e0c6810aa897f6f96e55c7df84852143a9329758
Freddie Brown <fred@noser.net>
2020-12-03 11:17:46 +0000

    clean up

--- src/day3.py
+++ src/day3.py
@@ -4,11 +4,11 @@ def p1(grid, x_step, y_step):
     max_x = len(grid[0])
     max_y = len(grid)
     trees = 0
+
     while y < max_y:
         if x >= max_x:
             x = x - max_x
-        space = grid[y][x]
-        if space == '#':
+        if grid[y][x] == '#':
             trees += 1
         x += x_step
         y += y_step
@@ -24,12 +24,6 @@ def p2(grid):
     r5 = p1(grid, 1, 2)
     return r1 * r2 * r3 * r4 * r5
 
-
-def print_grid(grid):
-    for y in grid:
-        for x in y:
-            print(x, end = '')
-        print("")
 
 grid = []
 with open('data/day3.txt') as f:
